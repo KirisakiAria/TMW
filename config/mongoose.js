3d979a4972dcec7b2d50e4e5f6037da0ab3f6d15
LGT <xiaoli350791904@hotmail.com>
2017-03-17 11:49:44 +0800

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@ var config = require('config-lite');
 var mongoose = require('mongoose');
 
 module.exports = function() {
-	var db = mongoose.connect(config.mongodb);
+	var db = mongoose.createConnection();
+	db.openSet(config.mongodb);
 	require("../models/increment.model");
 	require("../models/news.server.model");
 	return db;
