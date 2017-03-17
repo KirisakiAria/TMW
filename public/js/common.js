6e056a2e057ee0dd4a8487f727a9756adef7ae53
kirisakiaria <thereshegoes@qq.com>
2017-03-17 21:43:35 +0800

    update

--- public/js/common.js
+++ public/js/common.js
@@ -8,16 +8,8 @@ $(function() {
 		loading: true,
 		loadingParentElement: 'body',
 		loadingClass: 'animsition-loading',
-		loadingInner: '<div class="svg-container" id="container">' +
-			'<svg id="loader" width="100%" height="100%" viewBox="0 0 200 200" preserveAspectRatio="xMidYMid meet">'+
-		'<path id="jump" fill="none" stroke="#383845" stroke-width="10" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M47.5,94.3c0-23.5,19.9-42.5,44.5-42.5s44.5,19,44.5,42.5" />' +
-		'<g stroke="#383845" stroke-width="1">' +
-		'<ellipse id="circleL" fill="none" stroke-miterlimit="10" cx="47.2" cy="95.6" rx="10.7" ry="2.7" />' +
-		'<ellipse id="circleR" fill="none" stroke-miterlimit="10" cx="136.2" cy="95.6" rx="10.7" ry="2.7" />' +
-		'</g>' +
-		'</svg>' +
-		'</div>',
-		timeout: false,
+		loadingInner:'<img src="../img/loader.gif">',
+	 	timeout: false,
 		timeoutCountdown: 5000,
 		onLoadEvent: true,
 		browser: ['animation-duration', '-webkit-animation-duration'],
