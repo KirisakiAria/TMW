8c3b7cda25239c3dbaca1fae48ceaa36dae48a8d
LGT <xiaoli350791904@hotmail.com>
2017-03-17 12:26:46 +0800

    up

--- config/default.js
+++ config/default.js
@@ -5,6 +5,6 @@ module.exports = {
     key: 'TMW',
     maxAge: 2592000000
   },
-  //mongodb: 'mongodb://KirisakiAria:T!*AT2^ugFvY415C@localhost:27017/TMW?authSource=admin'
-  mongodb: 'mongodb://localhost:27017/TMW'
+  mongodb: 'mongodb://KirisakiAria:T!*AT2^ugFvY415C@localhost:27017/TMW?authSource=admin'
+  // mongodb: 'mongodb://localhost:27017/TMW'
 };
