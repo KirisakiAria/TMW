3543740ffd90aaf6aa34977095315ce8e27a0975
me <thereshegoes@qq.com>
2017-04-04 12:05:40 +0800

    update

--- config/default.js
+++ config/default.js
@@ -6,7 +6,7 @@ const configs = {
 		key: 'TMW',
 		maxAge: 2592000000
 	},
-	// mongodb: 'mongodb://KirisakiAria:T!*AT2^ugFvY415C@localhost:27017/TMW?authSource=admin'
-	mongodb: 'mongodb://localhost:27017/TMW'
+	mongodb: 'mongodb://kirisakiaria:1395317533--++@localhost:27017/TMW?authSource=admin'
+	// mongodb: 'mongodb://localhost:27017/TMW'
 }
 module.exports = configs;
